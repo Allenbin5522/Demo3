aaf95a2e32290da20836f147147c271be877cb62
JavaDemo01 <dllo@dllodeMac-mini-121.local>
2017-02-27 20:44:19 +0800

    第一次改动

---
+++ src/java03/Java03.java
@@ -0,0 +1,11 @@
+package java03;
+
+/**
+ * Created by dllo on 17/2/27.
+ */
+public class Java03 {
+    public static void main(String[] args) {
+        System.out.println("99999");
+        System.out.println("8888");
+    }
+}
